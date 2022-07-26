d9bcd3e7da095221a740238d4aa09e7aac90a0af
Shubham Pitekar <65482186+shuence@users.noreply.github.com>
2022-07-26 19:42:34 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Aug 08, 2022',
+  birthdate: 'Aug 10, 2022',
   name: "Momo 🤍"
 };
 
